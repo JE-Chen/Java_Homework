258ea6a0d96d7814651e9163fbb8f4d02a388f47
JE-Chen <zenmailman@gmail.com>
2020-11-18 10:34:45 +0800

    add example
    add example

--- src/Test/IA.java
+++ src/Test/IA.java
@@ -1,10 +1,19 @@
 package Test;
 
 public interface IA {
-    public int var = 0;
+
+    public int test1 = 0;
+
+    public static int test2 = 1;
+
+    static int test3 = 2;
+
+    static int test4 = 2;
+
+    public static final double pi = 3.14;
 
     private void func() {
-
+        System.out.println(pi);
     }
 
 }
