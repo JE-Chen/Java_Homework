b15372ee7598c6ce3eab5f914114647886263ec0
JE-Chen <zenmailman@gmail.com>
2021-04-22 10:58:51 +0800

    change Message class logic

--- src/main/java/networking_programming/thread/homework/Message.java
+++ src/main/java/networking_programming/thread/homework/Message.java
@@ -8,28 +8,28 @@ public class Message {
     private final ConcurrentLinkedQueue<Integer> integerConcurrentLinkedQueue = new ConcurrentLinkedQueue<>();
 
     synchronized void putValue() {
-        while (integerConcurrentLinkedQueue.size() == 5) {
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        while (integerConcurrentLinkedQueue.size() < 5) {
+            int randNumber = new Random().nextInt(100);
+            integerConcurrentLinkedQueue.add(randNumber);
+            System.out.println(randNumber + "\t(Put random number)");
+            this.notify();
         }
-        int randNumber = new Random().nextInt(100);
-        integerConcurrentLinkedQueue.add(randNumber);
-        System.out.println(randNumber + "\t(Put random number)");
-        this.notify();
+        try {
+            this.wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     synchronized void getValue() {
-        while (integerConcurrentLinkedQueue.size() == 0) {
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        while (integerConcurrentLinkedQueue.size() > 0) {
+            System.out.println(integerConcurrentLinkedQueue.poll() + "\t Get Number");
+            this.notify();
         }
-        System.out.println(integerConcurrentLinkedQueue.poll() + "\t Get Number");
-        this.notify();
+        try {
+            this.wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/networking_programming/thread/homework/Readme.md
@@ -0,0 +1,4 @@
+### 使用說明 
+* 開啟 cmd 或 powershell 並 cd 或 set-location 至檔案所在資料夾
+* java -jar 檔案.jar 會執行程式
+* 中斷請使用 ctrl + c 中斷指令
